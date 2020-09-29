7fbfd178cb01c3c4da2d3ac41ca8d491530332e4
Thiago Tavares <tvttavares@gmail.com>
2020-09-29 09:46:03 -0400

    Initial commit

---
+++ src/main/java/com/evoluum/EvoluumApplication.java
@@ -0,0 +1,13 @@
+package com.evoluum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoluumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoluumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evoluum/EvoluumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evoluum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoluumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
