b32c2586a4a9aa5f417158709915fe060800522c
Thiago Tavares <tvttavares@gmail.com>
2020-09-30 13:04:52 -0400

    Implementacao Swagger na API

---
+++ src/main/java/com/evoluum/config/swagger/SwaggerLinks.java
@@ -0,0 +1,10 @@
+package com.evoluum.config.swagger;
+
+public class SwaggerLinks {
+
+	public static final String TAG_LOCALIZACOES = "Localizaçoes";
+	public static final String DESC_CONTROLLER_JSON = "Endpoint de todos os municípios com retorno no formato JSON";
+	public static final String DESC_CONTROLLER_CSV = "Endpoint de todos os municípios com retorno no formato CSV";
+	public static final String DESC_CIDADE_BY_NAME = "Endpoint de busca do id da cidade pelo nome";
+
+}
--- src/main/java/com/evoluum/controller/LocalizacaoController.java
+++ src/main/java/com/evoluum/controller/LocalizacaoController.java
@@ -1,5 +1,10 @@
 package com.evoluum.controller;
 
+import static com.evoluum.config.swagger.SwaggerLinks.DESC_CIDADE_BY_NAME;
+import static com.evoluum.config.swagger.SwaggerLinks.DESC_CONTROLLER_CSV;
+import static com.evoluum.config.swagger.SwaggerLinks.DESC_CONTROLLER_JSON;
+import static com.evoluum.config.swagger.SwaggerLinks.TAG_LOCALIZACOES;
+
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -17,8 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.evoluum.model.dto.LocalizacaoDTO;
 import com.evoluum.service.LocalizacaoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/api/localizacao")
+@Api(tags = TAG_LOCALIZACOES)
 public class LocalizacaoController {
 
 	private Logger logger = LoggerFactory.getLogger(LocalizacaoController.class);
@@ -27,6 +36,7 @@ public class LocalizacaoController {
 	private LocalizacaoService localizacaoService;
 
 	@GetMapping("/todos/json")
+	@ApiOperation(value = DESC_CONTROLLER_JSON)
 	public ResponseEntity<List<LocalizacaoDTO>> getTodosOsDadosJSON(HttpServletResponse response) {
 		logger.info("Iniciando requisição de todos os dados em formato Json");
 		List<LocalizacaoDTO> dados = localizacaoService.getTodosOsDados(response);
@@ -35,6 +45,7 @@ public class LocalizacaoController {
 	}
 
 	@GetMapping("/todos/csv")
+	@ApiOperation(value = DESC_CONTROLLER_CSV)
 	public ResponseEntity<Object> getTodosOsDadosCSV(HttpServletResponse response) {
 		logger.info("Iniciando requisição de todos os dados em formato CSV");
 		localizacaoService.getTodosOsDadosCSV(response);
@@ -44,6 +55,7 @@ public class LocalizacaoController {
 	}
 
 	@GetMapping("/municipio/{nomeCidade}")
+	@ApiOperation(value = DESC_CIDADE_BY_NAME)
 	public ResponseEntity<String> getIdMunicipio(@PathVariable String nomeCidade) {
 		logger.info("Iniciando requisição para nome de cidade.");
 		String idCidade = localizacaoService.getIdMunicipio(nomeCidade.trim());
